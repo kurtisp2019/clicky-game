8db75113e57560aeb6bd68a8ed02e2cab6dd06c1
kurtisp <kurtis.pace2011@gmail.com>
2019-04-29 20:36:41 -0600

    Updates

---
+++ precache-manifest.476b6876f2436b6896bcc3b19bc97428.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "488f132f992ceba956a0c460c3fcf46b",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "3527126ffe999bc3c9fd",
+    "url": "/clicky-game/static/js/2.f30538a5.chunk.js"
+  },
+  {
+    "revision": "117ad9886dbac5a0fd68",
+    "url": "/clicky-game/static/js/main.a1e2a2a3.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
